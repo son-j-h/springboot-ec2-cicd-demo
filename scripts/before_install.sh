47a3d3863a5d3cf385d46d47ce4a6e0e9f3865aa
alecyoun <55869925+alecyoun@users.noreply.github.com>
2025-08-14 03:24:28 +0900

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -10,3 +10,5 @@ sudo chown ec2-user:ec2-user /home/ec2-user/app
 if ! command -v java >/dev/null 2>&1; then
   sudo yum -y install java-17-amazon-corretto-headless || sudo dnf -y install java-17-amazon-corretto-headless || true
 fi
+
+# systemd 관련 코드 완전히 제거됨!
